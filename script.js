caab5c24508892d42819dc5d9855aef656d5f450
Colin Tindle <ampersand@underscore.space>
2017-08-05 16:28:27 +0100

    Added swipe to go back/forward

--- script.js
+++ script.js
@@ -36,6 +36,10 @@
 
   let today, prev, next
 
+  const image = document.querySelector('IMG#dilbert')
+  const prevLink = document.querySelector('A#prev')
+  const nextLink = document.querySelector('A#next')
+
   window.addEventListener('load', function(e) {
     function updateDates() {
       const locHash = window.location.hash.substring(2,12)
@@ -44,10 +48,6 @@
         today = selectedDate()
         prev = formatDate(dateAdd(today, -1))
         next = formatDate(dateAdd(today, 1))
-
-        const image = document.querySelector('IMG#dilbert')
-        const prevLink = document.querySelector('A#prev')
-        const nextLink = document.querySelector('A#next')
 
         fetch(`https://d.op11.co.uk/images/${formatDate(today)}`).then(res => {
           if (!res.ok) {
@@ -58,6 +58,8 @@
         }).then(blob => {
           var imageURL = URL.createObjectURL(blob)
           image.src = imageURL
+          image.style.display = 'block'
+          image.style.opacity = 1
           prevLink.href = `#/${prev}`
           nextLink.href = `#/${next}`
         }).catch(err => {
@@ -82,4 +84,51 @@
     updateDates()
     window.addEventListener('hashchange', updateDates)
   })
+
+  image.addEventListener('touchstart', touchStart)
+  image.addEventListener('touchmove', touchMove)
+  image.addEventListener('touchend', touchEnd)
+
+  image.addEventListener('mousedown', touchStart)
+  image.addEventListener('mousemove', touchMove)
+  image.addEventListener('mouseup', touchEnd)
+
+  let swipe, swipeStart, swipeCurrent, swipeDiff, box
+
+  function touchStart(e) {
+    console.log('started')
+    swipe = true
+    swipeStart = e.pageX || e.touches[0].pageX
+    swipeCurrent = swipeStart
+    box = image.getBoundingClientRect()
+    console.log(box)
+
+    e.preventDefault()
+  }
+
+  function touchMove(e) {
+    if (swipe) {
+      swipeCurrent = e.pageX || e.touches[0].pageX
+      swipeDiff = swipeCurrent-swipeStart
+      image.style.transform = `translateX(${swipeDiff}px)`
+      image.style.opacity = 1 - Math.pow(Math.abs(swipeDiff) / box.width, 0.5)
+    }
+  }
+
+  function touchEnd(e) {
+    console.log(prevLink.href, nextLink.href)
+    console.log('stopped')
+    swipe = false
+    const swipePC = swipeDiff / box.width
+    if (swipePC > 0.35) {
+      image.style.display = 'none'
+      window.location = prevLink.href
+    } else if (swipePC < -0.35) {
+      image.style.display = 'none'
+      window.location = nextLink.href
+    }
+
+    image.style.opacity = 1
+    image.style.transform = `translateX(0px)`
+  }
 })();
